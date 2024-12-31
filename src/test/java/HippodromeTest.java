7894fcc14871ba965435f3cdcb2d1974cfc6cd9b
Ivan <zimin.ivann.1998@gmail.com>
2024-12-31 21:32:52 +0400

    Комментарий об ошибке

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -50,6 +50,8 @@ class HippodromeTest {
         for (Horse horse : horses){
             Mockito.verify(horse).move();
         }
+//        Если тестовый метод move в классе HippodromeTest заканчивается ошибкой: "org.mockito.exceptions.base.MockitoException:
+//Mockito cannot mock this class: class Horse.", нужно поменять Java до версии 21
     }
 
     @Test
