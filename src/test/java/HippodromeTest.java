b29a2dc13c95fc0c88b4250514dd8a741d41d749
Ivan <zimin.ivann.1998@gmail.com>
2024-12-31 20:41:09 +0400

    Метод move проходит тест

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 class HippodromeTest {
 
